3eb64783fb3af2243691785524de36e8f7abacfb
seb <69323547+sebbthepleb@users.noreply.github.com>
2022-07-07 13:12:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-https://sebbthepleb.github.io/seb.github.io/
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Sebb.</title>
+    <link
+      rel="icon"
+      href="https://cdn.glitch.me/e390f856-debd-4b82-a262-1f65555dc33b%2Fs.png?v=1634652999083"
+    />
+    <meta name="theme-color" content="#ffffff" />
+
+    <link href="styling.css" rel="stylesheet" />
+  </head>
+  <h1>Seb</h1>
+  <div class="box">lorem ipsum do,lor sit amet</div>
+</html>
