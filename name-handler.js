6a36fce454392c4f1d4f447e8ac682029191e165
seb <69323547+sebbthepleb@users.noreply.github.com>
2022-07-08 23:24:26 +0800

    Update name-handler.js

--- name-handler.js
+++ name-handler.js
@@ -5,4 +5,13 @@ function nameSwap() {
   } else {
     x.innerHTML = "Seb";
   }
+  
+  if (document.getElementById("pfp").src.endsWith('assets/profile.png') == true)
+  { 
+    document.getElementById("pfp").src = "assets/lgbt-profile.png";
+  } 
+  else if (document.getElementById("pfp").src.endsWith('assets/lgbt-profile.png') == true) 
+  { 
+    document.getElementById("pfp").src = "assets/profile.png"; 
+  }
 }
