48a68b1391a7e7cb6e26e6a9769b3643963f402e
seb <69323547+sebbthepleb@users.noreply.github.com>
2022-07-10 11:26:08 +0800

    added document.title change

--- name-handler.js
+++ name-handler.js
@@ -1,17 +1,26 @@
 function nameSwap() {
   var x = document.getElementById("name");
   if (x.innerHTML === "Seb") {
-    x.innerHTML = "Sasha";
-  } else {
-    x.innerHTML = "Seb";
-  }
+      x.innerHTML = "Sasha";
+    } else {
+      x.innerHTML = "Seb";
+    }
   
   if (document.getElementById("pfp").src.endsWith('assets/profile.png') == true)
-  { 
-    document.getElementById("pfp").src = "assets/lgbt-profile.png";
-  } 
+    { 
+      document.getElementById("pfp").src = "assets/lgbt-profile.png";
+    } 
   else if (document.getElementById("pfp").src.endsWith('assets/lgbt-profile.png') == true) 
-  { 
-    document.getElementById("pfp").src = "assets/profile.png"; 
-  }
+    { 
+      document.getElementById("pfp").src = "assets/profile.png"; 
+    }
+  
+  if (document.title === "Seb")
+    {
+      document.title = "Sasha";
+    }
+  else
+    {
+      document.title = "Seb";
+    }
 }
